010daf1f8b0d30276f377af41be6fd582d83623d
afivamay <afivamay14@gmail.com>
2025-10-15 22:20:14 +0700

    modifikasi 2

--- SiakadWhile03.java
+++ SiakadWhile03.java
@@ -16,20 +16,21 @@ public class SiakadWhile03 {
                 continue;
             }
 
-            if (nilai > 80)
-                System.out.println("Nilai huruf: A - Bagus, pertahankan nilainya!");
-            else if (nilai > 73)
-                System.out.println("Nilai huruf: B+");
-            else if (nilai > 65)
-                System.out.println("Nilai huruf: B");
-            else if (nilai > 60)
-                System.out.println("Nilai huruf: C+");
-            else if (nilai > 50)
-                System.out.println("Nilai huruf: C");
-            else if (nilai > 39)
-                System.out.println("Nilai huruf: D");
-            else
-                System.out.println("Nilai huruf: E");
+            if (nilai > 80 && nilai <= 100) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A - Bagus, pertahankan nilainya!");
+            } else if (nilai > 73 && nilai <= 80) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
+            } else if (nilai > 65 && nilai <= 73) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
+            } else if (nilai > 60 && nilai <= 65) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
+            } else if (nilai > 50 && nilai <= 60) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
+            } else if (nilai > 39 && nilai <= 50) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
+            } else {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
+            }
 
             i++;
         }
