f0cf77ef66d0147e42743c6e0f30d990c5922dac
afivamay <afivamay14@gmail.com>
2025-10-15 21:51:29 +0700

    percobaan 2

---
+++ SiakadWhile03.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class SiakadWhile03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nilai, jml, i = 0;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jml = sc.nextInt();
+
+        while (i < jml) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai = sc.nextInt();
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid! Masukkan ulang.\n");
+                continue;
+            }
+
+            if (nilai > 80)
+                System.out.println("Nilai huruf: A - Bagus, pertahankan nilainya!");
+            else if (nilai > 73)
+                System.out.println("Nilai huruf: B+");
+            else if (nilai > 65)
+                System.out.println("Nilai huruf: B");
+            else if (nilai > 60)
+                System.out.println("Nilai huruf: C+");
+            else if (nilai > 50)
+                System.out.println("Nilai huruf: C");
+            else if (nilai > 39)
+                System.out.println("Nilai huruf: D");
+            else
+                System.out.println("Nilai huruf: E");
+
+            i++;
+        }
+        sc.close();
+    }
+}
