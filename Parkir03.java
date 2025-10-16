03c77f6444a01843e168a048f4835a9c7c041613
afivamay <afivamay14@gmail.com>
2025-10-16 08:38:53 +0700

    Tugas 2

---
+++ Parkir03.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Parkir03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jenis, durasi;
+        int total = 0;
+
+        do {
+            System.out.print("\nMasukkan jenis kendaraan (1=Mobil, 2=Motor, 0=Selesai): ");
+            jenis = sc.nextInt();
+
+            if (jenis == 0)
+                break;
+            System.out.print("Masukkan durasi parkir (jam): ");
+            durasi = sc.nextInt();
+
+            int biaya;
+            if (durasi > 5)
+                biaya = 12500;
+            else if (jenis == 1)
+                biaya = 3000 * durasi;
+            else
+                biaya = 2000 * durasi;
+            total += biaya;
+            System.out.println("Biaya parkir kendaraan ini: Rp " + biaya);
+        } while (true);
+        System.out.println("\nTotal pembayaran parkir hari ini: Rp " + total);
+        sc.close();
+    }
+}
